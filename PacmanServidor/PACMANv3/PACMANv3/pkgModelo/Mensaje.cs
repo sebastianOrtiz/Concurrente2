efbf6417a9f8cdd51f9f245520d3daf1e29a6add
ShebisX <shebisx1@hotmail.com>
2015-11-16 19:47:14 -0500

    Se crea la clase mensaje,
    se cambia el nombre de las soluciones por conveniencia.

---
+++ PacmanServidor/PACMANv3/PACMANv3/pkgModelo/Mensaje.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PACMANv3.pkgModelo
+{
+    [Serializable()]
+    class Mensaje
+    {
+        private string nombre;
+        private string mensaje;
+
+        public Mensaje(string nombre, string mensaje)
+        {
+            this.nombre = nombre;
+            this.mensaje = mensaje;
+        }
+
+        /// <summary>
+        /// Accesor y mutador de el atributo Nombre
+        /// </summary>
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        /// <summary>
+        /// Accesor y mutador de el atributo Mensaje
+        /// </summary>
+        public string Mensaje
+        {
+            get { return mensaje; }
+            set { mensaje = value; }
+        }
+    }
+}
