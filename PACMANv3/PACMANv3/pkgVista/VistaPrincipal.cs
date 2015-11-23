1d5f73370e708b0cd509534b782bb929d37d9a5f
ShebisX <shebisx1@hotmail.com>
2015-11-23 14:31:43 -0500

    Conexión sockets

--- PACMANv3/PACMANv3/pkgVista/VistaPrincipal.cs
+++ PACMANv3/PACMANv3/pkgVista/VistaPrincipal.cs
@@ -12,11 +12,16 @@ using PACMANv3.pkgVista;
 using System.Threading;
 using Newtonsoft.Json;
 using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Reflection;
 using System.Media;
+using System.Net;
+using System.Net.Sockets;
 
-namespace PACMANv3 {
-    public partial class Form1 : Form {
+namespace PACMANv3
+{
+    public partial class Form1 : Form
+    {
         private Juego juego;
         private List<Mapa> listadeMapasDificil;
         private List<Mapa> listadeMapasMedio;
@@ -28,7 +33,8 @@ namespace PACMANv3 {
         private SoundPlayer soundFondo;
 
 
-        public Form1() {
+        public Form1()
+        {
             InitializeComponent();
             cmbSeleccionarDificultad.SelectedIndex = 0;
             listadeMapasDificil = new List<Mapa>();
@@ -41,88 +47,126 @@ namespace PACMANv3 {
             cargarDatosJugadores();
             cargarNombresDeMapas();
             cargarMapas();
+
+            /* Conexión socket(envio y recibo)Socket sc = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint direccion = new IPEndPoint(IPAddress.Loopback, 1339);
+
+            try
+            {
+                sc.Connect(direccion);            
+                Console.WriteLine("Conectado con exito");
+                Mensaje m = new Mensaje("cliente", "hola!!");
+                BinaryFormatter serializer = new BinaryFormatter();
+                NetworkStream st = new NetworkStream(sc);
+                serializer.Serialize(st, m);
+                m = (Mensaje)serializer.Deserialize(st);
+                Console.WriteLine("" + m.Nombre);
+                Console.WriteLine("" + m.Texto);
+                st.Close();
+                sc.Close();
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine("Error: {0}", error.ToString());
+            }*/
         }
 
-        private void escribirJSONJugadores() {
+        private void escribirJSONJugadores()
+        {
             String js = JsonConvert.SerializeObject(datosJugadores);
             StreamWriter sw = File.CreateText("./../../ArchivosConf/DatosUsuarios/HistoricoDeUsuarios.data");
             sw.Write(js);
             sw.Close();
         }
 
-        private void cargarDatosJugadores() {
+        private void cargarDatosJugadores()
+        {
             //LEYENDO JSON
             StreamReader sr;
             sr = File.OpenText("./../../ArchivosConf/DatosUsuarios/HistoricoDeUsuarios.data");
             String entrada = sr.ReadToEnd();
-            if (entrada != "") {
+            if (entrada != "")
+            {
                 this.datosJugadores = JsonConvert.DeserializeObject<List<DatosJugador>>(entrada);
             }
             sr.Close();
 
         }
 
-        private void Form1_Load(object sender, EventArgs e) {
+        private void Form1_Load(object sender, EventArgs e)
+        {
             carYReproducir();
         }
 
-        private void carYReproducir() {
+        private void carYReproducir()
+        {
             this.soundFondo = new SoundPlayer(Properties.Resources.nocheDeAcosador);
             this.soundFondo.Load();
             this.soundFondo.PlayLooping();
         }
 
-        private void cargarNombresDeMapas() {
+        private void cargarNombresDeMapas()
+        {
             StreamReader sr = new StreamReader("./../../ArchivosConf/Config/nombreMapasDificil.conf");
             String linea;
             linea = sr.ReadLine();
-            while (linea != null) {
+            while (linea != null)
+            {
                 nombresDeMapasDificil.Add(linea);
                 linea = sr.ReadLine();
             }
             sr = new StreamReader("./../../ArchivosConf/Config/nombreMapasMedio.conf");
             linea = sr.ReadLine();
-            while (linea != null) {
+            while (linea != null)
+            {
                 nombresDeMapasMedio.Add(linea);
                 linea = sr.ReadLine();
             }
 
             sr = new StreamReader("./../../ArchivosConf/Config/nombreMapasFacil.conf");
             linea = sr.ReadLine();
-            while (linea != null) {
+            while (linea != null)
+            {
                 nombresDeMapasFacil.Add(linea);
                 linea = sr.ReadLine();
             }
             sr.Close();
         }
 
-        private void cargarMapas() {
+        private void cargarMapas()
+        {
             //LEYENDO JSON
             StreamReader sr;
-            foreach (String nombreMapa in nombresDeMapasDificil) {
+            foreach (String nombreMapa in nombresDeMapasDificil)
+            {
                 sr = File.OpenText("./../../ArchivosConf/Maps/" + nombreMapa + ".map");
                 String entrada = sr.ReadToEnd();
-                if (entrada != "") {
+                if (entrada != "")
+                {
                     Mapa nMap = JsonConvert.DeserializeObject<Mapa>(entrada);
                     nMap.MatrizDiseño = new Celda[nMap.Filas, nMap.Columnas];
                     nMap.crearMapa();
                     listadeMapasDificil.Add(nMap);
                 }
             }
-            foreach (String nombreMapa in nombresDeMapasMedio) {
+            foreach (String nombreMapa in nombresDeMapasMedio)
+            {
                 sr = File.OpenText("./../../ArchivosConf/Maps/" + nombreMapa + ".map");
                 String entrada = sr.ReadToEnd();
-                if (entrada != "") {
+                if (entrada != "")
+                {
                     Mapa nMap = JsonConvert.DeserializeObject<Mapa>(entrada);
                     nMap.MatrizDiseño = new Celda[nMap.Filas, nMap.Columnas];
                     nMap.crearMapa();
                     listadeMapasMedio.Add(nMap);
                 }
             }
-            foreach (String nombreMapa in nombresDeMapasFacil) {
+            foreach (String nombreMapa in nombresDeMapasFacil)
+            {
                 sr = File.OpenText("./../../ArchivosConf/Maps/" + nombreMapa + ".map");
                 String entrada = sr.ReadToEnd();
-                if (entrada != "") {
+                if (entrada != "")
+                {
                     Mapa nMap = JsonConvert.DeserializeObject<Mapa>(entrada);
                     nMap.MatrizDiseño = new Celda[nMap.Filas, nMap.Columnas];
                     nMap.crearMapa();
@@ -131,35 +175,47 @@ namespace PACMANv3 {
             }
         }
 
-        private void btnAceptarConfigInicial_Click(object sender, EventArgs e) {
-            if (txtNombreJu.Text != null && cmbSeleccionarDificultad.SelectedIndex > -1) {
+        private void btnAceptarConfigInicial_Click(object sender, EventArgs e)
+        {
+            if (txtNombreJu.Text != null && cmbSeleccionarDificultad.SelectedIndex > -1)
+            {
                 String dificultad = cmbSeleccionarDificultad.SelectedItem.ToString();
                 DatosJugador nuevoJugador = new DatosJugador(txtNombreJu.Text, dificultad);
                 Boolean entradaPorVoz;
-                if (rBtnVoz.Checked) {
+                if (rBtnVoz.Checked)
+                {
                     entradaPorVoz = true;
-                } else {
+                }
+                else
+                {
                     entradaPorVoz = false;
                 }
 
-                if (dificultad == "Facil") {
+                if (dificultad == "Facil")
+                {
                     juego = new Juego(listadeMapasFacil, dificultad, nuevoJugador, entradaPorVoz, (int)nudVidasPacman.Value, (int)nudHPPacman.Value);
-                } else if (dificultad == "Medio") {
+                }
+                else if (dificultad == "Medio")
+                {
                     juego = new Juego(listadeMapasMedio, dificultad, nuevoJugador, entradaPorVoz, (int)nudVidasPacman.Value, (int)nudHPPacman.Value);
-                } else if (dificultad == "Dificil") {
+                }
+                else if (dificultad == "Dificil")
+                {
                     juego = new Juego(listadeMapasDificil, dificultad, nuevoJugador, entradaPorVoz, (int)nudVidasPacman.Value, (int)nudHPPacman.Value);
 
                 }
 
                 VistaJuego vj = new VistaJuego();
                 vj.definirEntrada(entradaPorVoz);
-                if (entradaPorVoz) {
+                if (entradaPorVoz)
+                {
                     vj.iniciarReconocedor();
                 }
                 vj.crearJuego(this.juego);
                 DialogResult res = vj.ShowDialog();
 
-                if (res == DialogResult.OK) {
+                if (res == DialogResult.OK)
+                {
                     carYReproducir();
                     DatosJugador jugadorEntrante = vj.Juego.DatosJugador;
                     jugadorEntrante.Fecha = DateTime.Now.ToString();
@@ -170,17 +226,21 @@ namespace PACMANv3 {
                     this.listarDatosJugadores();
                     this.cargarMapas();
                 }
-            } else {
+            }
+            else
+            {
                 MessageBox.Show("No hay nombre o nivel de dificultad seleccionado");
             }
         }
 
-        private void listarDatosJugadores() {
+        private void listarDatosJugadores()
+        {
             String datNom = "";
             String datdif = "";
             String datpts = "";
             String datfecha = "";
-            for (int i = 0; i < this.datosJugadores.Count; i++) {
+            for (int i = 0; i < this.datosJugadores.Count; i++)
+            {
                 datNom += (i + 1) + "- " + this.datosJugadores.ElementAt(i).Nombre + "\n";
                 datdif += this.datosJugadores.ElementAt(i).Dificultad + "\n";
                 datpts += this.datosJugadores.ElementAt(i).Puntaje + " pts\n";
@@ -193,27 +253,38 @@ namespace PACMANv3 {
 
         }
 
-        private void btnCrearMapa_Click(object sender, EventArgs e) {
+        private void btnCrearMapa_Click(object sender, EventArgs e)
+        {
             VistaCreacionDeMapas nuevaVen = new VistaCreacionDeMapas();
             DialogResult res = nuevaVen.ShowDialog();
 
-            if (res == DialogResult.OK) {
+            if (res == DialogResult.OK)
+            {
                 Mapa nMapa = nuevaVen.Mapa;
-                if (nMapa.Dificultad == "Dificil") {
+                if (nMapa.Dificultad == "Dificil")
+                {
                     listadeMapasDificil.Add(nMapa);
-                } else if (nMapa.Dificultad == "Medio") {
+                }
+                else if (nMapa.Dificultad == "Medio")
+                {
                     listadeMapasMedio.Add(nMapa);
-                } else if (nMapa.Dificultad == "Facil") {
+                }
+                else if (nMapa.Dificultad == "Facil")
+                {
                     listadeMapasFacil.Add(nMapa);
                 }
             }
         }
 
-        private void ordenarDescendente() {
+        private void ordenarDescendente()
+        {
             DatosJugador[] arrJug = this.datosJugadores.ToArray();
-            for (int i = 0; i < arrJug.Length; i++) {
-                for (int j = i + 1; j < arrJug.Length; j++) {
-                    if (arrJug[i].Puntaje < arrJug[j].Puntaje) {
+            for (int i = 0; i < arrJug.Length; i++)
+            {
+                for (int j = i + 1; j < arrJug.Length; j++)
+                {
+                    if (arrJug[i].Puntaje < arrJug[j].Puntaje)
+                    {
                         DatosJugador aux = arrJug[i];
                         arrJug[i] = arrJug[j];
                         arrJug[j] = aux;
@@ -224,20 +295,23 @@ namespace PACMANv3 {
             this.datosJugadores.AddRange(arrJug);
         }
 
-        private void btnAceptarDatos_Click(object sender, EventArgs e) {
+        private void btnAceptarDatos_Click(object sender, EventArgs e)
+        {
             escribirJSONJugadores();
             this.panelPuntajes.Visible = false;
             this.panelConfigInicio.Visible = true;
         }
 
-        private void btnIntPuntajes_Click(object sender, EventArgs e) {
+        private void btnIntPuntajes_Click(object sender, EventArgs e)
+        {
             this.ordenarDescendente();
             this.listarDatosJugadores();
             this.panelConfigInicio.Visible = false;
             this.panelPuntajes.Visible = true;
         }
 
-        private void btnEditarMapa_Click(object sender, EventArgs e) {
+        private void btnEditarMapa_Click(object sender, EventArgs e)
+        {
             VistaEdicionDeMapas ve = new VistaEdicionDeMapas();
             List<String> lista = new List<string>();
             List<Mapa> todosLosMapas = new List<Mapa>();
@@ -250,33 +324,45 @@ namespace PACMANv3 {
             ve.cargarMapasEnElEditor(lista, todosLosMapas);
             DialogResult res = ve.ShowDialog();
 
-            if (res == DialogResult.OK) {
+            if (res == DialogResult.OK)
+            {
                 Mapa nuevoMapa = ve.MapaNuevo;
                 Mapa[] arrayMapas;
-                if (nuevoMapa.Dificultad == "Dificil") {
+                if (nuevoMapa.Dificultad == "Dificil")
+                {
                     arrayMapas = listadeMapasDificil.ToArray();
-                    for (int i = 0; i < arrayMapas.Length; i++) {
-                        if (arrayMapas[i].Nombre.Equals(nuevoMapa.Nombre)) {
+                    for (int i = 0; i < arrayMapas.Length; i++)
+                    {
+                        if (arrayMapas[i].Nombre.Equals(nuevoMapa.Nombre))
+                        {
                             arrayMapas[i] = nuevoMapa;
                             break;
                         }
                     }
                     listadeMapasDificil.Clear();
                     listadeMapasDificil.AddRange(arrayMapas);
-                } else if (nuevoMapa.Dificultad == "Medio") {
+                }
+                else if (nuevoMapa.Dificultad == "Medio")
+                {
                     arrayMapas = listadeMapasMedio.ToArray();
-                    for (int i = 0; i < arrayMapas.Length; i++) {
-                        if (arrayMapas[i].Nombre.Equals(nuevoMapa.Nombre)) {
+                    for (int i = 0; i < arrayMapas.Length; i++)
+                    {
+                        if (arrayMapas[i].Nombre.Equals(nuevoMapa.Nombre))
+                        {
                             arrayMapas[i] = nuevoMapa;
                             break;
                         }
                     }
                     listadeMapasMedio.Clear();
                     listadeMapasMedio.AddRange(arrayMapas);
-                } else if (nuevoMapa.Dificultad == "Facil") {
+                }
+                else if (nuevoMapa.Dificultad == "Facil")
+                {
                     arrayMapas = listadeMapasFacil.ToArray();
-                    for (int i = 0; i < arrayMapas.Length; i++) {
-                        if (arrayMapas[i].Nombre.Equals(nuevoMapa.Nombre)) {
+                    for (int i = 0; i < arrayMapas.Length; i++)
+                    {
+                        if (arrayMapas[i].Nombre.Equals(nuevoMapa.Nombre))
+                        {
                             arrayMapas[i] = nuevoMapa;
                             break;
                         }
--- PacmanServidor/PACMANv3/PACMANv3/pkgVista/VistaPrincipal.cs
+++ PacmanServidor/PACMANv3/PACMANv3/pkgVista/VistaPrincipal.cs
@@ -12,11 +12,16 @@ using PACMANv3.pkgVista;
 using System.Threading;
 using Newtonsoft.Json;
 using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Reflection;
 using System.Media;
+using System.Net;
+using System.Net.Sockets;
 
-namespace PACMANv3 {
-    public partial class Form1 : Form {
+namespace PACMANv3
+{
+    public partial class Form1 : Form
+    {
         private Juego juego;
         private List<Mapa> listaDeMapas;
         private List<String> nombresDeMapas;
@@ -25,39 +30,73 @@ namespace PACMANv3 {
         private Mapa mapaAJugar;
 
 
-        public Form1() {
+        public Form1()
+        {
             InitializeComponent();
             cmbSeleccionarDificultad.SelectedIndex = 0;
-            
+
             this.listaDeMapas = new List<Mapa>();
             this.nombresDeMapas = new List<String>();
             this.datosJugadores = new List<DatosJugador>();
             cargarDatosJugadores();
             cargarNombresDeMapas();
             cargarMapas();
+
+            /* Conexión socket(envio y recibo)
+            Socket ss = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint direccion = new IPEndPoint(IPAddress.Loopback, 1339);
+            try
+            {
+                ss.Bind(direccion);
+                Console.WriteLine("Escuchando");
+                ss.Listen(1);
+                Console.WriteLine("Esperando");
+                Socket sc = ss.Accept();
+                BinaryFormatter serializer = new BinaryFormatter();
+                Mensaje m = null;
+                NetworkStream net = new NetworkStream(sc);
+                m = (Mensaje)serializer.Deserialize(net);
+                Console.WriteLine("" + m.Nombre);
+                Console.WriteLine("" + m.Texto);
+                m = new Mensaje("servidor","mundo!!");
+                serializer.Serialize(net, m);
+                net.Close();
+                sc.Close();
+                ss.Close();
+                Console.WriteLine("Cerrando conexion");
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine("Error: {0}", error.ToString());
+            }*/
         }
 
-        public void cargarMapasEnCombobox() {
+        public void cargarMapasEnCombobox()
+        {
 
-            foreach (String nombre in nombresDeMapas) {
+            foreach (String nombre in nombresDeMapas)
+            {
                 cmbSeleccionMapa.Items.Add(nombre);
                 cmbSeleccionMapa.SelectedIndex = 0;
             }
         }
 
-        private void escribirJSONJugadores() {
+        private void escribirJSONJugadores()
+        {
             String js = JsonConvert.SerializeObject(datosJugadores);
             StreamWriter sw = File.CreateText("./../../ArchivosConf/DatosUsuarios/HistoricoDeUsuarios.data");
             sw.Write(js);
             sw.Close();
         }
 
-        private void cargarDatosJugadores() {
+        private void cargarDatosJugadores()
+        {
             //LEYENDO JSON
             StreamReader sr;
             sr = File.OpenText("./../../ArchivosConf/DatosUsuarios/HistoricoDeUsuarios.data");
             String entrada = sr.ReadToEnd();
-            if (entrada != "") {
+            if (entrada != "")
+            {
                 this.datosJugadores = JsonConvert.DeserializeObject<List<DatosJugador>>(entrada);
             }
             sr.Close();
@@ -65,21 +104,25 @@ namespace PACMANv3 {
 
         }
 
-        private void Form1_Load(object sender, EventArgs e) {
+        private void Form1_Load(object sender, EventArgs e)
+        {
             carYReproducir();
         }
 
-        private void carYReproducir() {
+        private void carYReproducir()
+        {
             this.soundFondo = new SoundPlayer(Properties.Resources.nocheDeAcosador);
             this.soundFondo.Load();
             this.soundFondo.PlayLooping();
         }
 
-        private void cargarNombresDeMapas() {
+        private void cargarNombresDeMapas()
+        {
             StreamReader sr = new StreamReader("./../../ArchivosConf/Config/nombresMapas.conf");
             String linea;
             linea = sr.ReadLine();
-            while (linea != null) {
+            while (linea != null)
+            {
                 nombresDeMapas.Add(linea);
                 linea = sr.ReadLine();
             }
@@ -87,13 +130,16 @@ namespace PACMANv3 {
             this.cargarMapasEnCombobox();
         }
 
-        private void cargarMapas() {
+        private void cargarMapas()
+        {
             //LEYENDO JSON
             StreamReader sr;
-            foreach (String nombreMapa in nombresDeMapas) {
+            foreach (String nombreMapa in nombresDeMapas)
+            {
                 sr = File.OpenText("./../../ArchivosConf/Maps/" + nombreMapa + ".map");
                 String entrada = sr.ReadToEnd();
-                if (entrada != "") {
+                if (entrada != "")
+                {
                     Mapa nMap = JsonConvert.DeserializeObject<Mapa>(entrada);
                     nMap.MatrizDiseño = new Celda[nMap.Filas, nMap.Columnas];
                     nMap.crearMapa();
@@ -102,15 +148,21 @@ namespace PACMANv3 {
             }
         }
 
-        private void btnAceptarConfigInicial_Click(object sender, EventArgs e) {
-            if (cmbSeleccionMapa.SelectedIndex > -1) {
-                if (txtNombreJu.Text != null && cmbSeleccionarDificultad.SelectedIndex > -1) {
+        private void btnAceptarConfigInicial_Click(object sender, EventArgs e)
+        {
+            if (cmbSeleccionMapa.SelectedIndex > -1)
+            {
+                if (txtNombreJu.Text != null && cmbSeleccionarDificultad.SelectedIndex > -1)
+                {
                     String dificultad = cmbSeleccionarDificultad.SelectedItem.ToString();
                     DatosJugador nuevoJugador = new DatosJugador(txtNombreJu.Text, dificultad);
                     Boolean entradaPorVoz;
-                    if (rBtnVoz.Checked) {
+                    if (rBtnVoz.Checked)
+                    {
                         entradaPorVoz = true;
-                    } else {
+                    }
+                    else
+                    {
                         entradaPorVoz = false;
                     }
 
@@ -118,13 +170,15 @@ namespace PACMANv3 {
 
                     VistaJuego vj = new VistaJuego();
                     vj.definirEntrada(entradaPorVoz);
-                    if (entradaPorVoz) {
+                    if (entradaPorVoz)
+                    {
                         vj.iniciarReconocedor();
                     }
                     vj.crearJuego(this.juego);
                     DialogResult res = vj.ShowDialog();
 
-                    if (res == DialogResult.OK) {
+                    if (res == DialogResult.OK)
+                    {
                         carYReproducir();
                         DatosJugador jugadorEntrante = vj.Juego.DatosJugador;
                         jugadorEntrante.Fecha = DateTime.Now.ToString();
@@ -135,21 +189,27 @@ namespace PACMANv3 {
                         this.listarDatosJugadores();
                         this.cargarMapas();
                     }
-                } else {
+                }
+                else
+                {
                     MessageBox.Show("No hay nombre o nivel de dificultad seleccionado");
                 }
-            } else {
+            }
+            else
+            {
                 MessageBox.Show("Seleccione un Mapa");
             }
 
         }
 
-        private void listarDatosJugadores() {
+        private void listarDatosJugadores()
+        {
             String datNom = "";
             String datdif = "";
             String datpts = "";
             String datfecha = "";
-            for (int i = 0; i < this.datosJugadores.Count; i++) {
+            for (int i = 0; i < this.datosJugadores.Count; i++)
+            {
                 datNom += (i + 1) + "- " + this.datosJugadores.ElementAt(i).Nombre + "\n";
                 datdif += this.datosJugadores.ElementAt(i).Dificultad + "\n";
                 datpts += this.datosJugadores.ElementAt(i).Puntaje + " pts\n";
@@ -162,21 +222,27 @@ namespace PACMANv3 {
 
         }
 
-        private void btnCrearMapa_Click(object sender, EventArgs e) {
+        private void btnCrearMapa_Click(object sender, EventArgs e)
+        {
             VistaCreacionDeMapas nuevaVen = new VistaCreacionDeMapas();
             DialogResult res = nuevaVen.ShowDialog();
 
-            if (res == DialogResult.OK) {
+            if (res == DialogResult.OK)
+            {
                 //Mapa nMapa = nuevaVen.Mapa;
                 listaDeMapas.Add(nuevaVen.Mapa);
             }
         }
 
-        private void ordenarDescendente() {
+        private void ordenarDescendente()
+        {
             DatosJugador[] arrJug = this.datosJugadores.ToArray();
-            for (int i = 0; i < arrJug.Length; i++) {
-                for (int j = i + 1; j < arrJug.Length; j++) {
-                    if (arrJug[i].Puntaje < arrJug[j].Puntaje) {
+            for (int i = 0; i < arrJug.Length; i++)
+            {
+                for (int j = i + 1; j < arrJug.Length; j++)
+                {
+                    if (arrJug[i].Puntaje < arrJug[j].Puntaje)
+                    {
                         DatosJugador aux = arrJug[i];
                         arrJug[i] = arrJug[j];
                         arrJug[j] = aux;
@@ -187,20 +253,23 @@ namespace PACMANv3 {
             this.datosJugadores.AddRange(arrJug);
         }
 
-        private void btnAceptarDatos_Click(object sender, EventArgs e) {
+        private void btnAceptarDatos_Click(object sender, EventArgs e)
+        {
             escribirJSONJugadores();
             this.panelPuntajes.Visible = false;
             this.panelConfigInicio.Visible = true;
         }
 
-        private void btnIntPuntajes_Click(object sender, EventArgs e) {
+        private void btnIntPuntajes_Click(object sender, EventArgs e)
+        {
             this.ordenarDescendente();
             this.listarDatosJugadores();
             this.panelConfigInicio.Visible = false;
             this.panelPuntajes.Visible = true;
         }
 
-        private void btnEditarMapa_Click(object sender, EventArgs e) {
+        private void btnEditarMapa_Click(object sender, EventArgs e)
+        {
             VistaEdicionDeMapas ve = new VistaEdicionDeMapas();
             List<String> lista = new List<string>();
             List<Mapa> todosLosMapas = new List<Mapa>();
@@ -209,12 +278,15 @@ namespace PACMANv3 {
             ve.cargarMapasEnElEditor(lista, todosLosMapas);
             DialogResult res = ve.ShowDialog();
 
-            if (res == DialogResult.OK) {
+            if (res == DialogResult.OK)
+            {
                 Mapa nuevoMapa = ve.MapaNuevo;
                 Mapa[] arrayMapas;
                 arrayMapas = listaDeMapas.ToArray();
-                for (int i = 0; i < arrayMapas.Length; i++) {
-                    if (arrayMapas[i].Nombre.Equals(nuevoMapa.Nombre)) {
+                for (int i = 0; i < arrayMapas.Length; i++)
+                {
+                    if (arrayMapas[i].Nombre.Equals(nuevoMapa.Nombre))
+                    {
                         arrayMapas[i] = nuevoMapa;
                         break;
                     }
@@ -224,9 +296,12 @@ namespace PACMANv3 {
             }
         }
 
-        private void cmbSeleccionMapa_SelectionChangeCommitted(object sender, EventArgs e) {
-            foreach (Mapa mapa in this.listaDeMapas) {
-                if (mapa.Nombre.Equals(cmbSeleccionMapa.SelectedItem.ToString())) {
+        private void cmbSeleccionMapa_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            foreach (Mapa mapa in this.listaDeMapas)
+            {
+                if (mapa.Nombre.Equals(cmbSeleccionMapa.SelectedItem.ToString()))
+                {
                     this.mapaAJugar = mapa;
                     break;
                 }
