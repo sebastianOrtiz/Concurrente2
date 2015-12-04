a1878b7d16d7d13a1a4fbb5c978ff66fba23feb5
sebastianOrtiz <sebastianortiz989@gmail.com>
2015-12-04 16:25:10 -0500

    Cambia Estado a medio de transferencia global

--- PACMANv3/PACMANv3/pkgModelo/Estado.cs
+++ PACMANv3/PACMANv3/pkgModelo/Estado.cs
@@ -13,6 +13,19 @@ namespace PACMANv3.pkgModelo
         private List<Fantasma> fantasmas;
         private Biscocho[,] biscochos;
 
+        //Mensajes
+
+        private int id;
+        private string nombre;
+        private string texto;
+        private bool conectar;
+        private int direccion;
+        private int tEspera;
+
+        public Estado() {
+
+        }
+
         public Estado(List<PacMan> jugadores, List<Fantasma> fantasmas, Biscocho[,] biscochos)
         {
             this.jugadores = jugadores;
@@ -20,6 +33,14 @@ namespace PACMANv3.pkgModelo
             this.biscochos = biscochos;
         }
 
+        public Estado(int id, string nombre, string mensaje, int dir) {
+            this.id = id;
+            this.nombre = nombre;
+            this.texto = mensaje;
+            this.conectar = false;
+            this.direccion = dir;
+            this.tEspera = 0;
+        }
         public Biscocho[,] Biscochos
         {
             get { return biscochos; }
@@ -36,6 +57,54 @@ namespace PACMANv3.pkgModelo
         {
             get { return jugadores; }
             set { jugadores = value; }
+        }
+
+        /// <summary>
+        /// Accesor y mutador de el atributo Id
+        /// </summary>
+        public int Id {
+            get { return id; }
+            set { id = value; }
+        }
+
+        /// <summary>
+        /// Accesor y mutador de el atributo Nombre
+        /// </summary>
+        public string Nombre {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        /// <summary>
+        /// Accesor y mutador de el atributo Texto
+        /// </summary>
+        public string Texto {
+            get { return texto; }
+            set { texto = value; }
+        }
+
+        /// <summary>
+        /// Accesor y mutador de el atributo Conectar
+        /// </summary>
+        public bool Conectar {
+            get { return conectar; }
+            set { conectar = value; }
+        }
+
+        /// <summary>
+        /// Accesor y mutador de el atributo Direccion
+        /// </summary>
+        public int Direccion {
+            get { return direccion; }
+            set { direccion = value; }
+        }
+
+        /// <summary>
+        /// Accesor y mutador de el atributo TEspera
+        /// </summary>
+        public int TEspera {
+            get { return tEspera; }
+            set { tEspera = value; }
         }
     }
 }
--- PacmanServidor/PACMANv3/PACMANv3/pkgModelo/Estado.cs
+++ PacmanServidor/PACMANv3/PACMANv3/pkgModelo/Estado.cs
@@ -4,38 +4,101 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PACMANv3.pkgModelo
-{
+namespace PACMANv3.pkgModelo {
     [Serializable()]
-    public class Estado
-    {
+    public class Estado {
         private List<PacMan> jugadores;
         private List<Fantasma> fantasmas;
         private Biscocho[,] biscochos;
 
-        public Estado(List<PacMan> jugadores, List<Fantasma> fantasmas, Biscocho[,] biscochos)
-        {
+        //Mensajes
+
+        private int id;
+        private string nombre;
+        private string texto;
+        private bool conectar;
+        private int direccion;
+        private int tEspera;
+
+        public Estado() {
+
+        }
+
+        public Estado(List<PacMan> jugadores, List<Fantasma> fantasmas, Biscocho[,] biscochos) {
             this.jugadores = jugadores;
             this.fantasmas = fantasmas;
             this.biscochos = biscochos;
         }
 
-        public Biscocho[,] Biscochos
-        {
+        public Estado(int id, string nombre, string mensaje, int dir) {
+            this.id = id;
+            this.nombre = nombre;
+            this.texto = mensaje;
+            this.conectar = false;
+            this.direccion = dir;
+            this.tEspera = 0;
+        }
+        public Biscocho[,] Biscochos {
             get { return biscochos; }
             set { biscochos = value; }
         }
 
-        public List<Fantasma> Fantasmas
-        {
+        public List<Fantasma> Fantasmas {
             get { return fantasmas; }
             set { fantasmas = value; }
         }
 
-        public List<PacMan> Jugadores
-        {
+        public List<PacMan> Jugadores {
             get { return jugadores; }
             set { jugadores = value; }
+        }
+
+        /// <summary>
+        /// Accesor y mutador de el atributo Id
+        /// </summary>
+        public int Id {
+            get { return id; }
+            set { id = value; }
+        }
+
+        /// <summary>
+        /// Accesor y mutador de el atributo Nombre
+        /// </summary>
+        public string Nombre {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        /// <summary>
+        /// Accesor y mutador de el atributo Texto
+        /// </summary>
+        public string Texto {
+            get { return texto; }
+            set { texto = value; }
+        }
+
+        /// <summary>
+        /// Accesor y mutador de el atributo Conectar
+        /// </summary>
+        public bool Conectar {
+            get { return conectar; }
+            set { conectar = value; }
+        }
+
+        /// <summary>
+        /// Accesor y mutador de el atributo Direccion
+        /// </summary>
+        public int Direccion {
+            get { return direccion; }
+            set { direccion = value; }
+        }
+
+        /// <summary>
+        /// Accesor y mutador de el atributo TEspera
+        /// </summary>
+        public int TEspera {
+            get { return tEspera; }
+            set { tEspera = value; }
         }
     }
 }
